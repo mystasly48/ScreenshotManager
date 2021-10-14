c33cf6faec6a8a139a42c3cfc0a1b04f6678fd18
Maisu <6381045+mystasly48@users.noreply.github.com>
2021-10-14 18:25:26 +0900

    Change directory when debug mode

--- ScreenshotManager/Utils/Settings.cs
+++ ScreenshotManager/Utils/Settings.cs
@@ -3,8 +3,12 @@ using System.IO;
 
 namespace ScreenshotManager.Utils {
   public static class Settings {
+#if DEBUG
+    public static string ProductName => "ScrenshotManagerDebug";
+#else
     public static string ProductName => "ScreenshotManager";
-    
+#endif
+
     public static string ScreenshotFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), ProductName);
     public static string SettingsFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), ProductName);
     
